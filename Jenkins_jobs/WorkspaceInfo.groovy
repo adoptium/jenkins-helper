233e9b8050c2898b58cfeec0d872f7eae86aaf1b
Shubham Verma <VermaSh@users.noreply.github.com>
2018-04-11 14:45:31 -0400

    Workspace info (#16)
    * New features
    - Now the job accepts timeout value as build parameter
    - The user can now chose to iterate over all machines, by leaving the project label empty
    - Changed sort order to descending
    - Now the user can launch the job on specific machines, provided it's online
    
    Issue #12
    
    Signed-off-by: Shubham Verma <shubhamv.sv@gmail.com>

--- Jenkins_jobs/WorkspaceInfo.groovy
+++ Jenkins_jobs/WorkspaceInfo.groovy
@@ -3,145 +3,102 @@ import jenkins.model.Jenkins;
 import hudson.model.Computer;
 
 clones = [:]
-List<String> newMachines = new ArrayList<String>();
 
 /* Iterates over all the online nodes in Jenkins and
  * prints contents of workspace folder along with
  * the space they occupy. With exception of tmp directory
+ * tmp directory is any directory with "@tmp" in the name
+ * 
+ * The nodes can be either passed in or grabbed from Jenkins
  */
-node {
-    stage('Print_Space_Monitoring_Data') {
-        NodeHelper nodeHelper = new NodeHelper();
+stage('Pepare_Space_Monitoring_Jobs') {
+    NodeHelper nodeHelper = new NodeHelper();
 
-        String projectLabel;
-        if (params.projectLabel.length() > 1) {
-            projectLabel = params.projectLabel;            
-        } else {
-            projectLabel = "all";
+    String projectLabel
+    if (params.projectLabel.length() > 1) {
+        projectLabel = params.projectLabel
+    } else {
+        projectLabel = "all"
+    }
+    
+    ArrayList<Computer> computers = new ArrayList<Computer>()
+    if (params.machines.length() > 1) {
+        String[] inputMachines = params.machines.split(",")
+        for (String inputMachine : inputMachines) {
+            computers.add(nodeHelper.getComputer(inputMachine))
         }
-
-        def currentInstance = Jenkins.getInstance();
-        Computer[] computers;
-
+    } else {
+        def currentInstance = Jenkins.getInstance()
         if (currentInstance != null) {
             computers = currentInstance.getComputers();
         }
+    }
 
-        for (Computer computer : computers) {
-            String machineName = computer.getName();
 
-            if (computer.isOnline() && computer.getName() != "") {
-                String kernelName = nodeHelper.getOsKernelInfo(computer.getName()).get(0).toLowerCase()
+    for (Computer computer : computers) {
+        String machineName = computer.getName();
 
-                if (projectLabel.equals("all") 
+        if (computer.isOnline() && computer.getName() != "") {
+            String kernelName = nodeHelper.getOsKernelInfo(computer.getName()).get(0).toLowerCase()
+
+            if (projectLabel.equals("all") 
                         || nodeHelper.getLabels(computer.getName()).contains(projectLabel)) {
-                
-                    String workspaceDirectory = nodeHelper.getHomeDirectoryPath(machineName);
+            
+                String workspaceDirectory = nodeHelper.getHomeDirectoryPath(machineName)
 
-                    switch (kernelName) {
-                        case 'linux':
-                            clones[machineName] = {
-                                node (machineName) {
-                                    workspaceDirectory += "/workspace"
+                String workspaceStatscmd
+                String subdirectoryStatscmd
+                String statOutputHeading
 
-                                    /* #!/bin/sh -e\n is there to keep the output to console as clean as
-                                     * possible
-                                     */
-                                    String workspaceStats = sh (
-                                            script: '#!/bin/sh -e\n' + 'du -sh ' + workspaceDirectory,
-                                            returnStdout: true).trim()
-
-                                    String subdirectories = sh (
-                                            script: '#!/bin/sh -e\n du -sh ' + workspaceDirectory + '/* | sort -h',
-                                            returnStdout: true).trim()
-
-                                    println beautify(machineName,workspaceDirectory, workspaceStats,subdirectories)
-
-                                    cleanWs()
-                                }
-                            }
-                            break;
-                        case 'aix':
-                            clones[machineName] = {
-                                node (machineName) {
-                                    workspaceDirectory += "/workspace"
-                                    
-                                    String workspaceStats = sh (
-                                            script: '#!/bin/sh -e\n' + 'du -sg ' + workspaceDirectory,
-                                            returnStdout: true).trim()
-
-                                    String subdirectories = sh (
-                                            script: '#!/bin/sh -e\n du -sg ' + workspaceDirectory + '/* | sort -n',
-                                            returnStdout: true).trim()
-
-                                    println beautify(machineName + 'in Gb',workspaceDirectory, workspaceStats,subdirectories)
-
-                                    cleanWs()
-                                }
-                            }
-                            break;
-                        case 'mac':
-                            clones[machineName] = {
-                                node (machineName) {
-                                    workspaceDirectory += "/workspace"
-
-                                    String workspaceStats = sh (
-                                            script: '#!/bin/sh -e\n' + 'du -sh ' + workspaceDirectory,
-                                            returnStdout: true).trim()
-
-                                    String subdirectories = sh (
-                                            script: '#!/bin/sh -e\n du -sh ' + workspaceDirectory + '/* | sort -n',
-                                            returnStdout: true).trim()
-
-                                    println beautify(machineName,workspaceDirectory, workspaceStats,subdirectories)
-
-                                    cleanWs()
-                                }
-                            }
-                            break;
-                        /* This is commented out because it takes way too long to return 
-                         * it isn't a top priority
-                         * dir /s /-c
-                         */
-                        case 'windows':
-                        //     clones[machineName] = {
-                        //         node(machineName) {
-                        //                 cleanWs()
-                        //                 NodeHelper nodeHelper = new NodeHelper();
-                        //                 workspaceDirectory += "\\workspace";
-                        //                 println workspaceDirectory
-                        //                 // TODO: get path to directory from the api
-                        //                 String workspaceStats = sh (
-                        //                         script: '#!/bin/sh -e\n'  'du -sh '  workspaceDirectory,
-                        //                         returnStdout: true).trim()
-
-                        //                 String script = 'set x; du -sh '  workspaceDirectory  '\\* | sort -rn';
-                        //                 String subdirectories = sh (
-                        //                         script: 'script',
-                        //                         returnStdout: true).trim()
-                                 
-                                 
-                        //                 println beautify(machineName,workspaceDirectory, workspaceStats,subdirectories);                                 
-                                 
-                        //             cleanWs()
-                        //             }
-                        //         }
-                        //     }
-                        //     break;
-                        case 'zos':
-                        default:
-                            println ("Support for ${kernelName} is yet to be implemented");
-                            break;
-                    }
+                switch (kernelName) {
+                    case 'linux':
+                        workspaceDirectory += "/workspace"
+                        workspaceStatscmd = '#!/bin/sh -e\n' + 'du -sh ' + workspaceDirectory
+                        subdirectoryStatscmd = '#!/bin/sh -e\n du -sh ' + workspaceDirectory + '/* | sort -hr'
+                        statOutputHeading = machineName
+                        break;
+                    case 'aix':
+                        workspaceDirectory += "/workspace"
+                        workspaceStatscmd = '#!/bin/sh -e\n' + 'du -sg ' + workspaceDirectory
+                        subdirectoryStatscmd = '#!/bin/sh -e\n du -sg ' + workspaceDirectory + '/* | sort -nr'
+                        statOutputHeading = machineName + ' in GB'
+                        break;
+                    case 'mac':
+                        workspaceDirectory += "/workspace"
+                        workspaceStatscmd = '#!/bin/sh -e\n' + 'du -sh ' + workspaceDirectory
+                        subdirectoryStatscmd = '#!/bin/sh -e\n du -sh ' + workspaceDirectory + '/* | sort -nr'
+                        statOutputHeading = machineName
+                        break;
+                    /* This is commented out because it takes way too long to return 
+                     * it isn't a top priority
+                     * dir /s /-c
+                     */
+                    case 'windows':
+                        // workspaceDirectory += "\\workspace"
+                        // workspaceStatscmd = '#!/bin/sh -e\n du -sh ' + workspaceDirectory
+                        // subdirectoryStatscmd = '#!/bin/sh -e\n du -sh '  workspaceDirectory  '\\* | sort -rn'
+                        // statOutputHeading = machineName
+                        // break;
+                    case 'zos':
+                    default:
+                        println ("Support for ${kernelName} is yet to be implemented");
+                        break;
                 }
+
+                setupParallelPipelines(
+                        timeOut,
+                        machineName,
+                        workspaceDirectory,
+                        workspaceStatscmd,
+                        subdirectoryStatscmd,
+                        statOutputHeading)
             }
         }
-        currentInstance = null;
-        computers = null;
-
-        cleanWs()
     }
+    currentInstance = null;
+    computers = null;
 }
+
 
 @NonCPS
 def beautify(machineName, workspaceDirectory, workspaceStats, subdirectories) {
@@ -158,10 +115,46 @@ def beautify(machineName, workspaceDirectory, workspaceStats, subdirectories) {
         }
     }
     output += "\n=======================================================================\n\n";
+    return output
 }
 
-
-timeout(time: params.timeout, unit: 'HOURS') {
-    parallel clones
+def errorMessage(String machineName, String message) {
+    String errorOutput = "\n\n****NEEDS_ATTENTION****"
+    errorOutput += "\n${machineName} failed"
+    errorOutput += "\nMessage:\n${message}"
+    errorOutput += "\n****NEEDS_ATTENTION****\n\n"
+    return errorOutput
 }
 
+def setupParallelPipelines(
+        timeOut,
+        machineName,
+        workspaceDirectory,
+        workspaceStatscmd,
+        subdirectoryStatscmd,
+        statOutputHeading
+        ) {
+
+    clones[machineName] = {
+        try {
+            timeout(time: params.timeout as Integer, unit: 'HOURS') {
+                node (machineName) 
+                {                    
+                    String workspaceStats = sh (
+                            script: workspaceStatscmd,
+                            returnStdout: true).trim()
+
+                    String subdirectories = sh (
+                            script: subdirectoryStatscmd,
+                            returnStdout: true).trim()
+
+                    println beautify(statOutputHeading, workspaceDirectory, workspaceStats,subdirectories)
+                }
+            }
+        } catch(err) {
+            error (errorMessage(machineName, err.message))
+        }
+    }
+}
+
+parallel clones
