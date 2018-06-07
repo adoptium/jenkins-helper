cd170979953ca5e5ce0e4c7e2a9d5e0f44e93123
Xiaoxiao Wang <xiaoxiao.wang@ibm.com>
2018-06-07 16:24:06 -0400

    Make four changes to helper
    - change the format for output print
    - add remote-fs specifically for osx
    - use already defined JENKINS-URL
    - define one username variable
    
    Signed-off-by: Xiaoxiao Wang <xiaoxiao.wang@ibm.com>

--- vars/Constants.groovy
+++ vars/Constants.groovy
@@ -1,17 +1,17 @@
 class Constants {
+    static final String USERNAME = "jenkins";
     static final String[] IGNORE_LABELS = ['.ignore'];
-    static final String REMOTE_FS = "/home/jenkins";
-    static final String WIN_REMOTE_FS = "C:\\Users\\jenkins";
+    static final String REMOTE_FS = "/home/${USERNAME}";
+    static final String WIN_REMOTE_FS = "C:\\Users\\${USERNAME}";
+    static final String OSX_REMOTE_FS = "/Users/${USERNAME}";
     
     // This the key that'll be used for SSHLauncher in CreateNewNode
     static final String SSH_CREDENTIAL_ID = "";
     
-    static final String SLAVE_JAR_LOCATION = "<jenkins_URL/>/jnlpJars/slave.jar";
+    static final String SLAVE_JAR_LOCATION = "${JENKINS_URL}jnlpJars/slave.jar";
     static final String WGET_SLAVE_JAR = "\"wget -q --no-check-certificate -O slave.jar ${SLAVE_JAR_LOCATION} ; java -jar slave.jar\"";
-    static final String SSH_COMMAND = "ssh -C -i ${SSH_KEY_LOCATION} <userName/>@";
+    static final String SSH_COMMAND = "ssh -C -i ${SSH_KEY_LOCATION} ${USERNAME}@";
     static final String SSH_KEY_LOCATION = "";
-
-    static final String JENKINS_URL = "https://ci.adoptopenjdk.net/";
 
     static final String OS_LABEL_FORMAT = "%s.%s.%s";
     static final String GENERIC_LABEL_FORMAT = "%s.%s";
