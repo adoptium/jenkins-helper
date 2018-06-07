191c254f51034aef7e0f621688dc35f7d95a33ac
Xiaoxiao Wang <xiaoxiao.wang@ibm.com>
2018-06-07 16:24:06 -0400

    change the username to j9build
    Signed-off-by: Xiaoxiao Wang <xiaoxiao.wang@ibm.com>

--- Jenkins_jobs/CreateNewNode.groovy
+++ Jenkins_jobs/CreateNewNode.groovy
@@ -16,20 +16,88 @@ node {
         String[] machines = params.machineNames.split(",")
         String[] machineIPs = params.machineIPs.split(",")
         String[] labels = params.labelStrings.split(",")
+        def FILES = []
+        git url: "git@github.ibm.com:Xiaoxiao-Wang/infra-can.git", credentialsId: Constants.SSH_CREDENTIAL_ID, branch:"INI"
+        FILES = sh (
+                    script: "cat ansible/playbooks/Infrastructure/dynamic_inventory.ini",
+                    returnStdout: true
+                ).trim()
+        def FILES_LIST = FILES.split("\\n")
+        echo "${FILES_LIST}"
+        for (machine in FILES_LIST){ 
+            def EXISTED = false;
+            def emptyMap = [:];
+            def halfmachine = "";
+            if (machine =~ /[^\s]+ \s*(\S+)\s*=\s*(.*)?(?:#|$)\S*/) {
+            if (machine.contains("#")) {
+                def location = machine.indexOf("#")
+                halfmachine = machine.substring(location + 1,machine.length())
+            } else {
+                halfmachine = ""
+            }
+            def splitLines = halfmachine.split(',')
+            splitLines.each {
+                if (it != "" && it != null){
+                    String[] parts = it.split("=",2);
+                    emptyMap.put(parts[0],parts[1])
+                }
+            }
+                for (aSlave in hudson.model.Hudson.instance.slaves) {
+                    if (machine.contains(aSlave.name)){
+                    EXISTED = true;
+
+                    if (emptyMap.get("Labels") != null && emptyMap.get("Labels") != aSlave.getLabelString()) {
+                        aSlave.setLabelString(emptyMap.get("Labels"))
+                    }
+
+                    if (emptyMap.get("Executor") != null && emptyMap.get("Executor") != aSlave.getNumExecutors()) {
+                        aSlave.setNumExecutors(emptyMap.get("Executor").toInteger())
+                    }
+
+                    if (emptyMap.get("Description")!= null && emptyMap.get("Description") != aSlave.getDescriptor()){
+                        aSlave.setNodeDescription(emptyMap.get("Description"))
+                    }
+                    break;
+                }
+                }
+                def name = "";
+                if (!EXISTED){
+                    machine.find (/\S+.ibm.com/){
+                        def location = it.indexOf(".")
+                        it = it.substring(0,location)
+                        name = it
+                    }
+                    machines = machines + "${name}"
+                    def hostLocation = machine.indexOf("=")
+                    def machineIP
+                    if (machine.contains("#")) {
+                        def location = machine.indexOf("#")
+                        machineIP = machine.substring(hostLocation + 1,location -1)
+                    } else {
+                        location = machine.length()
+                        machineIP = machine.substring(hostLocation + 1,location)
+                    }
+                    
+                    machineIPs = machineIPs + machineIP
+                    if (emptyMap.get("Labels") != null) {
+                        labels = labels + emptyMap.get("Labels");
+                    }
+                }
+            }
+        }
 
         def launcher
         String remoteFS
         String newMachineLabels
         String os
         String newMachineName
-
         for (int index = 0; index < machineIPs.length; index++) {
 
             if (nodeHelper.getComputer(machines[index]) != null) {
-                println "Machine(${machines[index]}) already exists."
+                println "Machine: '${machines[index]}' already exists."
             } else {
                 if (Integer.parseInt(machineIPs[index].split("\\.")[0]) == 10) {
-                    launcher = new CommandLauncher(Constants.SSH_COMMAND + "${machineIPs[index]} " + Constants.WGET_SLAVE_JAR);
+                    launcher = new CommandLauncher(Constants.SSH_COMMAND + "${machineIPs[index]} " + "\"wget -q --no-check-certificate -O slave.jar ${JENKINS_URL}jnlpJars/slave.jar ; java -jar slave.jar\"");
                     remoteFS = Constants.REMOTE_FS;
                 } else if (machines[index].contains("win")) {
                     launcher = new JNLPLauncher("", "", new jenkins.slaves.RemotingWorkDirSettings(false, "", "remoting", false));
@@ -41,9 +109,13 @@ node {
                                 params.SSHCredentialId.isEmpty() ? Constants.SSH_CREDENTIAL_ID : params.SSHCredentialId,
                                 null, null, null, null, null, null, null,
                                 new NonVerifyingKeyVerificationStrategy());
-                    remoteFS = Constants.REMOTE_FS;
+                    if (machines[index].contains("osx")){
+                        remoteFS = Constants.OSX_REMOTE_FS;
+                    } else {
+                        remoteFS = Constants.REMOTE_FS;
+                    }
                 }
-                
+
                 newMachineLabels = labels[index%labels.length]
 
                 newMachineName = nodeHelper.addNewNode(
@@ -70,7 +142,7 @@ node {
                    (Jenkins.getInstance().getComputer(newMachineName)).connect(false);
                 }
 
-                println "Machine ${newMachineName} was added with following labels ${newMachineLabels}";
+                println "Machine:'${newMachineName}'\nlabels: '${newMachineLabels}'\nremote root directory: '${remoteFS}'";
             }
         }
 
