95ee5203d81bf4ac05be81c25c0eaa295c772082
Shubham Verma <shubhamv.sv@gmail.com>
2018-06-18 15:01:56 -0400

    Changes requested in the review - Removed extra white spaces - Removed redundant error function - Fixed comment typo
    Signed-off-by: Shubham Verma <shubhamv.sv@gmail.com>

--- Jenkins_jobs/WorkspaceInfo.groovy
+++ Jenkins_jobs/WorkspaceInfo.groovy
@@ -12,97 +12,97 @@ result = [:]
  * 
  * The nodes can be either passed in or grabbed from Jenkins
  */
-    stage('Pepare_Space_Monitoring_Jobs') {
-        NodeHelper nodeHelper = new NodeHelper();
+stage('Pepare_Space_Monitoring_Jobs') {
+    NodeHelper nodeHelper = new NodeHelper();
 
-        String projectLabel
-        if (params.projectLabel.length() > 1) {
-            projectLabel = params.projectLabel
-        } else {
-            projectLabel = "all"
+    String projectLabel
+    if (params.projectLabel.length() > 1) {
+        projectLabel = params.projectLabel
+    } else {
+        projectLabel = "all"
+    }
+    
+    ArrayList<Computer> computers = new ArrayList<Computer>()
+    if (params.machines.length() > 1) {
+        String[] inputMachines = params.machines.split(",")
+        for (String inputMachine : inputMachines) {
+            computers.add(nodeHelper.getComputer(inputMachine))
         }
-        
-        ArrayList<Computer> computers = new ArrayList<Computer>()
-        if (params.machines.length() > 1) {
-            String[] inputMachines = params.machines.split(",")
-            for (String inputMachine : inputMachines) {
-                computers.add(nodeHelper.getComputer(inputMachine))
-            }
-        } else {
-            def currentInstance = Jenkins.getInstance()
-            if (currentInstance != null) {
-                computers = currentInstance.getComputers();
-            }
+    } else {
+        def currentInstance = Jenkins.getInstance()
+        if (currentInstance != null) {
+            computers = currentInstance.getComputers();
         }
+    }
 
 
-        for (Computer computer : computers) {
-            String machineName = computer.getName();
+    for (Computer computer : computers) {
+        String machineName = computer.getName();
 
-            if (computer.isOnline() && computer.getName() != "") {
-                String kernelName = nodeHelper.getOsKernelInfo(computer.getName()).get(0).toLowerCase()
+        if (computer.isOnline() && computer.getName() != "") {
+            String kernelName = nodeHelper.getOsKernelInfo(computer.getName()).get(0).toLowerCase()
 
-                if (projectLabel.equals("all") 
-                            || nodeHelper.getLabels(computer.getName()).contains(projectLabel)) {
-                
-                    String workspaceDirectory = nodeHelper.getHomeDirectoryPath(machineName)
+            if (projectLabel.equals("all")
+                        || nodeHelper.getLabels(computer.getName()).contains(projectLabel)) {
 
-                    String workspaceStatscmd
-                    String subdirectoryStatscmd
-                    String statOutputHeading
+                String workspaceDirectory = nodeHelper.getHomeDirectoryPath(machineName)
 
-                    switch (kernelName) {
-                        case 'linux':
-                            workspaceDirectory += "/workspace"
-                            workspaceStatscmd = '#!/bin/sh -e\n' + 'du -sh ' + workspaceDirectory
-                            subdirectoryStatscmd = '#!/bin/sh -e\n du -sh ' + workspaceDirectory + '/* | sort -hr'
-                            statOutputHeading = machineName
-                            break;
-                        case 'aix':
-                            workspaceDirectory += "/workspace"
-                            workspaceStatscmd = '#!/bin/sh -e\n' + 'du -sg ' + workspaceDirectory
-                            subdirectoryStatscmd = '#!/bin/sh -e\n du -sg ' + workspaceDirectory + '/* | sort -nr'
-                            statOutputHeading = machineName + ' in GB'
-                            break;
-                        case 'mac':
-                            workspaceDirectory += "/workspace"
-                            workspaceStatscmd = '#!/bin/sh -e\n' + 'du -sh ' + workspaceDirectory
-                            subdirectoryStatscmd = '#!/bin/sh -e\n du -sh ' + workspaceDirectory + '/* | sort -nr'
-                            statOutputHeading = machineName
-                            break;
-                        /* This is commented out because it takes way too long to return 
-                         * it isn't a top priority
-                         * dir /s /-c
-                         */
-                        case 'windows':
-                            // workspaceDirectory += "\\workspace"
-                            // workspaceStatscmd = '#!/bin/sh -e\n du -sh ' + workspaceDirectory
-                            // subdirectoryStatscmd = '#!/bin/sh -e\n du -sh '  workspaceDirectory  '\\* | sort -rn'
-                            // statOutputHeading = machineName
-                            // break;
-                        case 'zos':
-                        default:
-                            result[machineName] = "Support for ${kernelName} is yet to be implemented"
-                            break;
-                    }
+                String workspaceStatscmd
+                String subdirectoryStatscmd
+                String statOutputHeading
 
-                    if (workspaceStatscmd != null && subdirectoryStatscmd != null) {
-                        setupParallelPipelines(
-                                timeOut,
-                                machineName,
-                                workspaceDirectory,
-                                workspaceStatscmd,
-                                subdirectoryStatscmd,
-                                statOutputHeading)
-                    } else {
+                switch (kernelName) {
+                    case 'linux':
+                        workspaceDirectory += "/workspace"
+                        workspaceStatscmd = '#!/bin/sh -e\n' + 'du -sh ' + workspaceDirectory
+                        subdirectoryStatscmd = '#!/bin/sh -e\n du -sh ' + workspaceDirectory + '/* | sort -hr'
+                        statOutputHeading = machineName
+                        break;
+                    case 'aix':
+                        workspaceDirectory += "/workspace"
+                        workspaceStatscmd = '#!/bin/sh -e\n' + 'du -sg ' + workspaceDirectory
+                        subdirectoryStatscmd = '#!/bin/sh -e\n du -sg ' + workspaceDirectory + '/* | sort -nr'
+                        statOutputHeading = machineName + ' in GB'
+                        break;
+                    case 'mac':
+                        workspaceDirectory += "/workspace"
+                        workspaceStatscmd = '#!/bin/sh -e\n' + 'du -sh ' + workspaceDirectory
+                        subdirectoryStatscmd = '#!/bin/sh -e\n du -sh ' + workspaceDirectory + '/* | sort -nr'
+                        statOutputHeading = machineName
+                        break;
+                    /* This is commented out because it takes way too long to return 
+                     * it isn't a top priority
+                     * dir /s /-c
+                     */
+                    case 'windows':
+                        // workspaceDirectory += "\\workspace"
+                        // workspaceStatscmd = '#!/bin/sh -e\n du -sh ' + workspaceDirectory
+                        // subdirectoryStatscmd = '#!/bin/sh -e\n du -sh '  workspaceDirectory  '\\* | sort -rn'
+                        // statOutputHeading = machineName
+                        // break;
+                    case 'zos':
+                    default:
                         result[machineName] = "Support for ${kernelName} is yet to be implemented"
-                    }
+                        break;
+                }
+
+                if (workspaceStatscmd != null && subdirectoryStatscmd != null) {
+                    setupParallelPipelines(
+                            timeOut,
+                            machineName,
+                            workspaceDirectory,
+                            workspaceStatscmd,
+                            subdirectoryStatscmd,
+                            statOutputHeading)
+                } else {
+                    result[machineName] = "Support for ${kernelName} is yet to be implemented"
                 }
             }
         }
-        currentInstance = null;
-        computers = null;
     }
+    currentInstance = null;
+    computers = null;
+}
 
 
 @NonCPS
@@ -121,14 +121,6 @@ def beautify(machineName, workspaceDirectory, workspaceStats, subdirectories) {
     }
     output += "\n=======================================================================\n\n";
     return output
-}
-
-def errorMessage(String machineName, String message) {
-    String errorOutput = "\n\n****NEEDS_ATTENTION****"
-    errorOutput += "\n${machineName} failed"
-    errorOutput += "\nMessage:\n${message}"
-    errorOutput += "\n****NEEDS_ATTENTION****\n\n"
-    return errorOutput
 }
 
 def setupParallelPipelines(
@@ -167,7 +159,7 @@ def setupParallelPipelines(
 parallel clones
 
 println "About to print error messages"
-String errors = "\n" \\ This is so that the first line after "Error" in the console
+String errors = "\n" // This is so that the first line after "Error" in the console
 for (Map<String, String> entry : result) {
     errors += entry.key + ":" + entry.value + "\n\n"
 }
